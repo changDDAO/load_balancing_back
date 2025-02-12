7735ebca93d8727de990a41c8edfd600e437dd22
changDDAO <younch8342@naver.com>
2025-02-12 22:40:46 +0900

    Initial commit

---
+++ src/main/java/com/changddao/load_balancing_back/LoadBalancingBackApplication.java
@@ -0,0 +1,13 @@
+package com.changddao.load_balancing_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancingBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancingBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/changddao/load_balancing_back/LoadBalancingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changddao.load_balancing_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancingBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
