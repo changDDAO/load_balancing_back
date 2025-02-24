33d0f1b0f1d31b2aca4d4393d0a051893bffd2d5
changDDAO <younch8342@naver.com>
2025-02-24 22:54:27 +0900

    feat: JPAQueryFactory @Bean 등록

---
+++ src/main/java/com/changddao/load_balancing_back/config/QueryDslConfig.java
@@ -0,0 +1,19 @@
+package com.changddao.load_balancing_back.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+    @PersistenceContext
+    EntityManager em;
+
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
