6af09537492a5e476e1af49dde0131c9aec5279f
changDDAO <younch8342@naver.com>
2025-02-24 21:21:35 +0900

    feat: @EnableJpaAuditing기능을 사용하여 각 모델들이 생성될 때, BaseTimeEntity를 상속받도록 한다.

---
+++ src/main/java/com/changddao/load_balancing_back/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.changddao.load_balancing_back.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
---
+++ src/main/java/com/changddao/load_balancing_back/entity/Member.java
@@ -0,0 +1,11 @@
+package com.changddao.load_balancing_back.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Member {
+}
