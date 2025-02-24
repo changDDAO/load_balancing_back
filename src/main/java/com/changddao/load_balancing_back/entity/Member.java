6d55bfc2e73f2160ca0ac4c112337fce3f36892f
changDDAO <younch8342@naver.com>
2025-02-24 21:54:38 +0900

    feat: Member Entity 컬럼 추가 및 편의메서드 양방향 유지될 수 있도록 변경

--- src/main/java/com/changddao/load_balancing_back/entity/Member.java
+++ src/main/java/com/changddao/load_balancing_back/entity/Member.java
@@ -1,11 +1,42 @@
 package com.changddao.load_balancing_back.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/*현재 가정은 한팀에 여러 멤버들이 속할 수 있는 것으로 가정한다.*/
 @Entity
 @NoArgsConstructor
 @Getter
-public class Member {
+public class Member extends BaseEntity{
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long memberId;
+    /*멤버 이름*/
+    private String name;
+    /*멤버 주소*/
+    @Embedded
+    private Address address;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+    @Builder
+    public Member(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    /*연관관계 편의메서드*/
+    public void setTeam(Team team) {
+        /*기존 팀과 관계 제거*/
+        if (this.team != null) {
+            this.team.getMembers().remove(this);
+        }
+        this.team = team;
+        /*양방향 관계 유지*/
+        if (team != null && !team.getMembers().contains(this)) {
+            team.getMembers().add(this);
+        }
+    }
 }
