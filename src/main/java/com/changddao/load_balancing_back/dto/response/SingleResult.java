c20ec274cc2f2211992ee68dcc29b9c6a595a49d
changDDAO <younch8342@naver.com>
2025-02-27 19:40:24 +0900

    feat: 결과 값을 반환할 단일 data 인지, 다중 data인지에 따라 Dto 나누기

--- src/main/java/com/changddao/load_balancing_back/dto/response/SingleResult.java
+++ src/main/java/com/changddao/load_balancing_back/dto/response/SingleResult.java
@@ -3,9 +3,8 @@ package com.changddao.load_balancing_back.dto.response;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 @Getter
 @Setter
-public class MultipleResult<T> extends Result{
-    List<T> datas;
+public class SingleResult<T> extends Result{
+    T data;
 }
