0238adc7d33d08385e5dc29ac8aae2def221ecc3
changDDAO <younch8342@naver.com>
2025-02-28 21:45:35 +0900

    refactor : ResponseService 구현체 갈아 끼우기

--- src/main/java/com/changddao/load_balancing_back/service/impl/ResponseServiceImpl.java
+++ src/main/java/com/changddao/load_balancing_back/service/impl/ResponseServiceImpl.java
@@ -10,24 +10,36 @@ import java.util.List;
 
 @Service
 public class ResponseServiceImpl implements ResponseService {
+    /*단일데이터 결과값 구현체*/
     @Override
     public <T> SingleResult<T> handleSingleResult(T data) {
-        return null;
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
     }
-
+    /*다중데이터 결과값 구현체*/
     @Override
     public <T> MultipleResult<T> handleListResult(List<T> list) {
-        return null;
+        MultipleResult<T> result = new MultipleResult<>();
+        result.setDatas(list);
+        setSuccessResult(result);
+        return result;
     }
-
+    /*성공결과 구현체*/
     @Override
     public Result handleSuccessResult() {
-        return null;
+        Result result = new Result();
+        setSuccessResult(result);
+        return result;
     }
-
+    /*실패결과 구현체*/
     @Override
     public Result handleFailResult(int code, String msg) {
-        return null;
+        Result result = new Result();
+        result.setMsg(msg);
+        result.setCode(code);
+        return result;
     }
     /*결과를 성공으로 setting 해줄 메서드*/
     private void setSuccessResult(Result result) {
