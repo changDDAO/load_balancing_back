e3c09d1e8469840f48d7291075bd8c98d8f30b62
changDDAO <younch8342@naver.com>
2025-03-03 20:19:43 +0900

    feat: MemberController API 작성 및 Postman으로 Request TEST

--- src/main/java/com/changddao/load_balancing_back/controller/MemberController.java
+++ src/main/java/com/changddao/load_balancing_back/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.changddao.load_balancing_back.dto.MemberDto;
 import com.changddao.load_balancing_back.dto.MemberSearchDto;
 import com.changddao.load_balancing_back.dto.response.SingleResult;
 import com.changddao.load_balancing_back.entity.Member;
+import com.changddao.load_balancing_back.repository.member.MemberRepository;
 import com.changddao.load_balancing_back.service.MemberService;
 import com.changddao.load_balancing_back.service.ResponseService;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,8 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final ResponseService responseService;
     private final MemberService memberService;
-
+    private final MemberRepository memberRepository;
+    /*멤버 전체 조회*/
     @GetMapping("/v1/members")
     public SingleResult<Page<MemberDto>> membersWithCond(MemberSearchDto cond,
                                                         @RequestParam(value ="page", defaultValue = "0") int page,
@@ -26,10 +28,31 @@ public class MemberController {
         PageRequest pageRequest = PageRequest.of(page, size);
         return responseService.handleSingleResult(memberService.findAllWithTeam(cond, pageRequest));
     }
+    /*멤버 단건 조회*/
+    @GetMapping("/v1/member/{id}")
+    public SingleResult<Member> findMember(@PathVariable("id") Long id) {
+        Member findMember = memberRepository.findById(id).orElseThrow(() -> new RuntimeException("찾고자 하는 멤버가 없습니다."));
+        return responseService.handleSingleResult(findMember);
+    }
+
+    /*멤버 가입*/
     @PostMapping("/v1/member/join")
     public SingleResult<Member> joinMember(@RequestBody Member member) {
         return responseService.handleSingleResult(memberService.joinMember(member));
     }
+    /*멤버 삭제*/
+    @DeleteMapping("/v1/member/{id}")
+    public SingleResult<Long> deleteMember(@PathVariable("id") Long id) {
+        return responseService.handleSingleResult(id);
+    }
+    /*멤버 정보 변경*/
+    @PutMapping("/v1/member{id}")
+    public SingleResult<Member> changeMember(@RequestBody Member member) {
+        Member findMember = memberRepository.findById(member.getMemberId()).orElseThrow(() -> new RuntimeException("찾고자하는 멤버가 없습니다."));
+        findMember.changeName(member.getName());
+        return responseService.handleSingleResult(findMember);
+    }
+
 
 
 }
