2f8e39ebbd4dc7ec0f637231a2c8a7d97040f607
changDDAO <younch8342@naver.com>
2025-03-22 16:57:33 +0900

    refactor: .env파일 설정 후, docker-compose.yml 재정의 및 local환경과 prod 환경 분리하기

--- src/main/java/com/changddao/load_balancing_back/LoadBalancingBackApplication.java
+++ src/main/java/com/changddao/load_balancing_back/LoadBalancingBackApplication.java
@@ -1,6 +1,5 @@
 package com.changddao.load_balancing_back;
 
-import com.changddao.load_balancing_back.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,9 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class LoadBalancingBackApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoadBalancingBackApplication.class, args);
-        System.out.printf("DB_USERNAME : %s", EnvConfig.get("DB_USERNAME"));
+    public static void main(String[] args) {SpringApplication.run(LoadBalancingBackApplication.class, args);
     }
 
 }
